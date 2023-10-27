ce4497e70ea147feca088e26a90f75495aed1e0b
aemstudentnumber2 <aemstudentnumber2@gmail.com>
2023-10-27 23:57:19 +0300

    fixed

--- ui.apps/src/main/content/jcr_root/apps/aemsitetest/clientlibs/testclientlib/js/foo.js
+++ ui.apps/src/main/content/jcr_root/apps/aemsitetest/clientlibs/testclientlib/js/foo.js
@@ -1,6 +1,6 @@
 function changeTitleIfImageIsBig() {
     var img = document.getElementById('image');
-    if(img.width > 800 || img.height > 800) {
+    if(img.width + img.height > 800) {
         var h1 = document.querySelector('h1');
    	    h1.innerHTML = h1.innerHTML + " big image";
     }
